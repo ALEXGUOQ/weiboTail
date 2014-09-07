7e2db343d6e54ac68e7525bb779f75cf60642788
colin <hitwhylz@163.com>
2014-09-07 11:17:05 +0800

    Initial Commit

---
+++ weiboTail/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  weiboTail
+//
+//  Created by Colin on 14-9-7.
+//  Copyright (c) 2014年 icephone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
